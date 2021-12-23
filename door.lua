1de11993d3fa58cd7878b40a25e10a6efb9e84fa
TheRedCreations <35812887+TheRedCreations@users.noreply.github.com>
2021-12-23 15:25:44 +0100

    Update door.lua

--- door.lua
+++ door.lua
@@ -4,17 +4,17 @@ os = require("os")
 door = component.os_rolldoorcontroller
 alarm = component.os_alarm
  
-local gate_iter = component.list('os_keypad')
-local keypad1, keypad2 = component.proxy(gate_iter()), component.proxy(gate_iter())
+local keypad1 = component.os_keypad
 alarm.setAlarm("area51_alert")
 alarm.setRange(0.5)
+color = 2
  
 customButtons = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "0", "ok"} 
-customButtonColor = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4} 
+customButtonColor = {color,color,color,color,color,color,color,color,color,color,color,color} 
 keypad1.setKey(customButtons, customButtonColor)
-keypad2.setKey(customButtons, customButtonColor)
  
 local pin = "0000"
+local tpin = "0001"
 local keypadInput = ""
  
 -- set this to true if you want to run the script as daemon
@@ -27,25 +27,26 @@ function updateDisplay()
     end
  
     keypad1.setDisplay(displayString, 7)
-    keypad2.setDisplay(displayString, 7)
 end
  
 function checkPin()
     if keypadInput == pin then
         keypad1.setDisplay("granted", 2)
-        keypad2.setDisplay("granted", 2)
         door.open()
         os.sleep(5)
         keypad1.setDisplay("CLOSING", 4)
-        keypad2.setDisplay("CLOSING", 4)
         door.close()
         os.sleep(2)
-    else
+    else if (keypadInput == tpin) then
+        keypad1.setDisplay("toggled", 2)
+        door.toggle()
+        os.sleep(2)
+        else
         keypad1.setDisplay("denied", 4)
-        keypad2.setDisplay("denied", 4)
         alarm.activate()
         os.sleep(5)
         alarm.deactivate()
+    end
     end    
     keypadInput = ""
     os.sleep(1)
@@ -73,4 +74,15 @@ event.listen("keypad", keypadEvent)
  
 -- clear keypad display
 keypad1.setDisplay("")
-keypad2.setDisplay("")
+if not runScriptInBackground then
+    -- sleep until the user interrupts the program with CTRL + C
+    local stopMe = false
+    event.listen("interrupted", function() stopMe = true; end)
+    while not stopMe do os.sleep(0.1) end
+
+    -- ignore keypad events on exit
+    event.ignore("keypad", keypadEvent)
+
+    -- show that the keypad is inactive
+    keypad1.setDisplay("inactive", 6)
+end
