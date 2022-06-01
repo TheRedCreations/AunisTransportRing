2eba068d28d10a63100826e3aba9f5081462a4a9
TheRedCreations <35812887+TheRedCreations@users.noreply.github.com>
2022-06-01 17:40:29 +0200

    Update blockdoor.lua

--- blockdoor.lua
+++ blockdoor.lua
@@ -1,19 +1,25 @@
 component = require("component")
 event = require("event")
 os = require("os")
-door = component.os_rolldoorcontroller
+keypad1 = component.os_keypad
+mag1=component.proxy("9c63aa07-ec9d-49ed-a6d6-614c44680127")
+mag2=component.proxy("c8c22e72-ffa8-42dc-ac96-ad66f3ad2337")
+mag3=component.proxy("c08082cf-6b40-4a79-aa0b-93f89b62194d")
+mag4=component.proxy("e56c6150-af54-4f4d-b5d7-0114aee6ac6e")
+door1=component.proxy("8e9491a1-c05a-4c1a-8559-c3513bf06b49")
+door2=component.proxy("60b4271b-f549-46fd-b811-ec51ad41cb73")
+door3=component.proxy("cfc14af6-82e3-4caf-b917-49d2558995e7")
+door4=component.proxy("81484f05-e354-472b-bf2b-439c81baeaeb")
 
-local gate_iter = component.list('os_keypad')
-local keypad1, keypad2 = component.proxy(gate_iter()), component.proxy(gate_iter())
 color = 2
  
 customButtons = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "0", "ok"} 
 customButtonColor = {color,color,color,color,color,color,color,color,color,color,color,color} 
 keypad1.setKey(customButtons, customButtonColor)
-keypad2.setKey(customButtons, customButtonColor)
  
-local pin = "0000" --pin for open the Rolldoor. It autocloses after 5 Sec
-local tpin = "0001" --pin for toggleing the Rolldoor
+local pin1 = "0000" --open rolldoors
+local pin2 = "0001" --close rolldoors
+local password = ""
 local time = 5 --time the Rolldoor remains open
 local keypadInput = ""
  
@@ -27,30 +33,28 @@ function updateDisplay()
     end
  
     keypad1.setDisplay(displayString, 7)
-    keypad2.setDisplay(displayString, 7)
 end
  
 function checkPin()
-    if keypadInput == pin then
-        keypad1.setDisplay("granted", 2)
-        keypad2.setDisplay("granted", 2)
-        door.open()
-        os.sleep(time)
-        keypad1.setDisplay("CLOSING", 4)
-        keypad2.setDisplay("CLOSING", 4)
-        door.close()
+    if keypadInput == pin1 then
+        keypad1.setDisplay("OPENING", 2)
+        door1.open()
+        door2.open()
+        door3.open()
+        door4.open()
         os.sleep(2)
-    else if (keypadInput == tpin) then
-        keypad1.setDisplay("toggled", 2)
-        keypad2.setDisplay("toggled", 2)
-        door.toggle()
+    else if keypadInput == pin2 then
+        keypad1.setDisplay("CLOSING", 4)
+        door1.close()
+        door2.close()
+        door3.close()
+        door4.close()
         os.sleep(2)
         else
         keypad1.setDisplay("denied", 4)
-        keypad2.setDisplay("denied", 4)
         os.sleep(5)
-    end
-    end    
+    end   
+  end
     keypadInput = ""
     os.sleep(1)
 end
@@ -71,9 +75,38 @@ function keypadEvent(eventName, address, button, button_label)
  
     updateDisplay()  
 end
+
+function keycardEvent(eventName, address, _, cardData, _, _, _)
+  
+  if(cardData==password) then
+  print("keycard used")
+   if(address == "9c63aa07-ec9d-49ed-a6d6-614c44680127") then
+        door1.open()
+        os.sleep(time)
+        door1.close()
+   end
+   if(address == "c8c22e72-ffa8-42dc-ac96-ad66f3ad2337") then
+        door2.open()
+        os.sleep(time)
+        door2.close()
+   end
+   if(address == "c08082cf-6b40-4a79-aa0b-93f89b62194d") then
+        door3.open()
+        os.sleep(time)
+        door3.close()
+   end
+   if(address == "e56c6150-af54-4f4d-b5d7-0114aee6ac6e") then
+        door4.open()
+        os.sleep(time)
+        door4.close()
+   end
+  end
+  
+ 
  
 -- listen to keypad events
 event.listen("keypad", keypadEvent)
+event.listen("magData", keycardEvent)
  
 -- clear keypad display
 keypad1.setDisplay("")
@@ -85,8 +118,8 @@ if not runScriptInBackground then
 
     -- ignore keypad events on exit
     event.ignore("keypad", keypadEvent)
+    event.ignore("magData", keycardEvent)
 
     -- show that the keypad is inactive
     keypad1.setDisplay("inactive", 6)
-    keypad2.setDisplay("inactive", 6)
 end
